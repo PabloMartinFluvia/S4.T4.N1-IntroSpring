4e15a03748faf32a8adb66d7beebecfd697a9e02
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-09-12 18:33:24 +0200

    N1 done

---
+++ S04T01N01MartinFluviaPablo/src/main/java/cat/itacademy/barcelonactiva/MartinFluvia/Pablo/s04/t01/n01/S04T01N01MartinFluviaPablo/controllers/HelloWorldController.java
@@ -0,0 +1,31 @@
+package cat.itacademy.barcelonactiva.MartinFluvia.Pablo.s04.t01.n01.S04T01N01MartinFluviaPablo.controllers;
+
+import cat.itacademy.barcelonactiva.MartinFluvia.Pablo.s04.t01.n01.S04T01N01MartinFluviaPablo.S04T01N01MartinFluviaPabloApplication;
+import org.slf4j.Logger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {  
+    
+    private final Logger LOG = S04T01N01MartinFluviaPabloApplication.LOG;
+    private String msg;
+    
+    
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom){        
+        msg = "Hola, " + nom + ". Estàs executant un projecte Maven";
+        LOG.info("Reacting to get HelloWorld");
+        return msg;
+    }
+    
+    @GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
+    public String saluda2(@PathVariable(required=false) String nom){
+        msg = "Hola, " + nom + ". Estàs executant un projecte Maven";
+        LOG.info("Reacting to get HelloWorld2");
+        return msg;
+    }
+}
