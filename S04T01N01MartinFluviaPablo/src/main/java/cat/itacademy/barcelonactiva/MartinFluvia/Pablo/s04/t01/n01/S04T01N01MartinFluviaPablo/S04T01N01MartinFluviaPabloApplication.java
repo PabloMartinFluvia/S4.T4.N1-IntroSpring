a538b28238880d741f09d5c7b7a0a7f0e90badc1
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-09-12 17:32:03 +0200

    import

---
+++ S04T01N01MartinFluviaPablo/src/main/java/cat/itacademy/barcelonactiva/MartinFluvia/Pablo/s04/t01/n01/S04T01N01MartinFluviaPablo/S04T01N01MartinFluviaPabloApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.MartinFluvia.Pablo.s04.t01.n01.S04T01N01MartinFluviaPablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01MartinFluviaPabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01MartinFluviaPabloApplication.class, args);
+	}
+
+}
