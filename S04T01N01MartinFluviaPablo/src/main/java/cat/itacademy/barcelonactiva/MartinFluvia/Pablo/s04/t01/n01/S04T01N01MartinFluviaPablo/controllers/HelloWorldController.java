148fc36c39521e035989a28c36c63bb204dd3c61
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-09-13 09:57:01 +0200

    clean code

--- S04T01N01MartinFluviaPablo/src/main/java/cat/itacademy/barcelonactiva/MartinFluvia/Pablo/s04/t01/n01/S04T01N01MartinFluviaPablo/controllers/HelloWorldController.java
+++ S04T01N01MartinFluviaPablo/src/main/java/cat/itacademy/barcelonactiva/MartinFluvia/Pablo/s04/t01/n01/S04T01N01MartinFluviaPablo/controllers/HelloWorldController.java
@@ -4,12 +4,10 @@ import cat.itacademy.barcelonactiva.MartinFluvia.Pablo.s04.t01.n01.S04T01N01Mart
 import java.util.Collections;
 import java.util.Map;
 import org.slf4j.Logger;
-import org.springframework.http.MediaType;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,17 +18,16 @@ public class HelloWorldController {
     
     
     @GetMapping(value="/HelloWorld")
-    public Map saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom){        
+    public Map<String,String> saluda(@RequestParam(value = "nom", defaultValue = "UNKNOWN") String nom){        
         msg = "Hola, " + nom + ". Estàs executant un projecte Maven";
         LOG.info("Reacting to get HelloWorld");
-        return Collections.singletonMap("a","b");
-        //return msg;        
+        return Collections.singletonMap("response",msg);
     }
     
     @GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
-    public String saluda2(@PathVariable(required=false) String nom){
+    public Map<String,String> saluda2(@PathVariable(required=false) String nom){
         msg = "Hola, " + nom + ". Estàs executant un projecte Maven";
         LOG.info("Reacting to get HelloWorld2");
-        return msg;
+        return Collections.singletonMap("response",msg);
     }
 }
